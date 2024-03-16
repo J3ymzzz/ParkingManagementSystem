69ec6c29f8bfc89583c422d9f4cb804f9188a31c
James Ryan Gregorio <126756124+J3ymzzz@users.noreply.github.com>
2024-03-16 22:11:10 +0800

    PrknMgmtSys

---
+++ ParkingMonitoringSystem/ParkingMonitoringSystem/Form1.cs
@@ -0,0 +1,50 @@
+namespace ParkingMonitoringSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
